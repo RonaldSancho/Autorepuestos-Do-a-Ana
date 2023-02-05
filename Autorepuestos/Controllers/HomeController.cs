67a5fe9220b7472c943544bd95ecc9b7110453ea
Pardo2273 <gaboX2273>
2023-02-05 12:19:35 -0600

    Adjunto parte del crud pedidos

--- Autorepuestos/Controllers/HomeController.cs
+++ Autorepuestos/Controllers/HomeController.cs
@@ -10,11 +10,13 @@ namespace Autorepuestos.Controllers
     {
         private readonly ILogger<HomeController> _logger;
         private readonly IUsuariosModel _usuariosModel;
+        private readonly IPedidosModel _PedidosModel;
 
-        public HomeController(ILogger<HomeController> logger, IUsuariosModel usuariosModel)
+        public HomeController(ILogger<HomeController> logger, IUsuariosModel usuariosModel, IPedidosModel pedidosModel)
         {
             _logger = logger;
             _usuariosModel = usuariosModel;
+            _PedidosModel = pedidosModel;
         }
 
         [HttpGet]
@@ -31,6 +33,49 @@ namespace Autorepuestos.Controllers
             else
                 return View("Index");
 
+        }
+
+        /*Pasar esto a otro controller*/
+        [HttpGet]
+        public ActionResult MostrarPedidoProveedores(PedidosEntities pedido)
+        {
+            return View(_PedidosModel.VerPedidos(pedido));
+        }
+
+        [HttpGet]
+        public ActionResult AgregarPedidoProveedores()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AgregarPedidoProveedores(PedidosEntities pedido)
+        {
+            _PedidosModel.Crear(pedido);
+            return View("MostrarPedidoProveedores");
+        }
+
+        [HttpGet]
+        public ActionResult EliminarPedido(int id)
+        {
+            _PedidosModel.eliminar(id);
+            return RedirectToAction("MostrarPedidoProveedores");
+
+        }
+
+
+        [HttpGet]
+        public ActionResult ModificarPedidoProveedores()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult ModificarPedidoProveedores(PedidosEntities pedido)
+        {
+            //este llamara a la interfaz
+            _PedidosModel.Editar(pedido);
+            return RedirectToAction("MostrarPedidoProveedores");
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
