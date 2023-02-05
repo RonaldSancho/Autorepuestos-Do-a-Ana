0746cb00cc0c9bd287e907c878ac0ce33c0bf1ff
Pardo2273 <gaboX2273>
2023-02-05 12:04:16 -0600

    Entidad Pedido

---
+++ Autorepuestos/Entities/PedidosEntities.cs
@@ -0,0 +1,18 @@
+﻿using static Mysqlx.Expect.Open.Types.Condition.Types;
+namespace Autorepuestos.Entities
+{
+    public class PedidosEntities
+    {
+        
+            public int IdPedido { get; set; }
+            public int IdProducto { get; set; }
+            public int IdFactura { get; set; }
+            public int Cantidad { get; set; }
+            public int IdProveedor { get; set; }
+            public int PrecioProducto { get; set; }
+            public int PrecioTotal { get; set; }
+            public int IdUsuario { get; set; }
+
+        
+    }
+}
